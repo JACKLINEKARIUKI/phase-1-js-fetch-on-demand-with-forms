a907605620d2a9b9a2226844caabb9f0951d723c
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-27 17:45:44 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // console.log(event);
+        const input = document.querySelector('input#searchByID');
+        // console.log(input.value);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())
+            .then((movie) => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = movie.title;
+            summary.innerText = movie.summary;
+            });
+        })
 }
 
 document.addEventListener('DOMContentLoaded', init);
